b5876a4fd6c9f396c78dc703ef340bef39798a89
PARINYA JAIHARN <s6201012620155@email.kmutnb.ac.th>
2020-08-17 22:55:47 +0700

    Create HelloJAVA class

---
+++ HelloJAVA.java
@@ -0,0 +1,4 @@
+public class HelloJAVA{                   //create class "HelloJAVA"
+public static void main(String []args){   //annouce function not return value
+System.out.println("Hello JAVA");         //print "Hello JAVA" on display
+}}
